c75386def633c18568fdc059d62606410d7a05e9
William Pohlman <william.a.pohlman@gmail.com>
2020-07-29 20:51:54 -0500

    part 2 of bonus mission

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: string[] = ["Habitat dome", "Drones", "Food containers", "Oxygen tanks"];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(item: string){
+    this.equipment.push(item);
   }
 
 }
